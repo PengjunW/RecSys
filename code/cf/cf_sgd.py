8514b89f388c06925f21494fc6e43dd5349688bb
wpj <wpj319@gmail.com>
2021-04-21 03:47:42 +0800

    complete the rec function.

--- code/cf/cf_sgd.py
+++ code/cf/cf_sgd.py
@@ -13,6 +13,7 @@ class BaselineCfBySGD(object):
 
     def fit(self, dataset):
         self.dataset = dataset
+        self.rating_matrix = self.dataset.pivot_table(index=["userId"], columns=["movieId"], values="rating")
         self.user_ratings = dataset.groupby(self.columns[0]).agg([list])[[self.columns[1], self.columns[2]]]
         self.item_ratings = dataset.groupby(self.columns[1]).agg([list])[[self.columns[0], self.columns[2]]]
         self.global_mean = self.dataset[self.columns[2]].mean()
@@ -42,6 +43,42 @@ class BaselineCfBySGD(object):
                 pass
             else:
                 yield uid, iid, real_rating, pred_rating
+
+    def predict_all(self, uid, item_ids):
+        for iid in item_ids:
+            try:
+                rating = self.predict(uid, iid)
+            except Exception as e:
+                pass
+            else:
+                yield uid, iid, rating
+
+    def recommendations(self, uid, filter_rule=None):
+        if not filter_rule:
+            item_ids = self.rating_matrix.columns
+        elif filter_rule == 'unhot':
+            count = self.rating_matrix.count()
+            item_ids = count.where(count > 10).dropna().index
+        elif filter_rule == 'rated':
+            user_rating = self.rating_matrix.loc[uid]
+            ids = user_rating[user_rating < 6].dropna().index
+            item_ids = user_rating.drop(ids).index
+        elif set(filter_rule) == set(['unhot', 'rated']):
+            count = self.rating_matrix.count()
+            ids1 = count.where(count > 10).dropna().index
+            user_rating = self.rating_matrix.loc[uid]
+            _ids = user_rating[user_rating < 6].dropna().index
+            ids2 = user_rating.drop(_ids).index
+            item_ids = set(ids1) & set(ids2)
+        else:
+            raise Exception('no such rule')
+
+        yield from self.predict_all(uid, item_ids)
+
+    def top_N_rs(self, uid, n, filter_rule=None):
+        # 基于预测评分进行排序
+        results = self.recommendations(uid, filter_rule)
+        return sorted(results, key=lambda x: x[2], reverse=True)[:n]
 
 
 def data_split(data_path, x=0.8):
@@ -95,8 +132,15 @@ def accuracy(predict_results, method='all'):
 
 if __name__ == '__main__':
     trainset, testset = data_split(DATA_PATH)
-    bcf = BaselineCfBySGD(500, 0.1, 0.1, ['userId', 'movieId', 'rating'])
+    bcf = BaselineCfBySGD(50, 0.1, 0.1, ['userId', 'movieId', 'rating'])
     bcf.fit(trainset)
     pred_results = bcf.test(testset)
     rmse, mae = accuracy(pred_results)
     print("rmse:", rmse, "  mas:", mae)
+    # 推荐电影
+    n = 5
+    uid = 1
+    filter_rule = ['unhot', 'rated']
+    results = np.array(bcf.top_N_rs(uid, n, filter_rule), dtype=np.int32)[:, 1]
+    print('推荐给 用户%d 的%d部电影ID:' % (uid, n))
+    print(results)
